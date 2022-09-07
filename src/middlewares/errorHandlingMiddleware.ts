1675865b9a346f1a1e673ca1500aca2f65b5eebf
farias-77 <gabrielfalves2003@gmail.com>
2022-09-07 16:40:16 -0300

    sign-in route tested

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 export default function errorHandlingMiddleware(error: any, req: Request, res: Response, next: NextFunction){
-    
+
     if(error.code === "conflict") return res.status(409).send(error.message);
+
+    if(error.code === "unauthorized") return res.status(401).send(error.message);
     
     return res.sendStatus(500);
 }
