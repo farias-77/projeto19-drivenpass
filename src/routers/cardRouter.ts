eaefdcc73c85c5b1a6c4dfadfc9f6e1577c8d56f
farias-77 <gabrielfalves2003@gmail.com>
2022-09-09 17:10:50 -0300

    get all cards by userId route (bug fix)

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -3,10 +3,10 @@ import tokenMiddleware from "../middlewares/tokenValidationMiddleware";
 import { cardCreationSchema } from "../schemas/cardSchemas";
 
 import { Router } from "express";
-import { insertCard } from "../controllers/cardController";
+import { getAllCardsByUserId, insertCard } from "../controllers/cardController";
 const router = Router();
 
 router.post("/card", schemaValidation(cardCreationSchema), tokenMiddleware, insertCard);
-router.get("/card", tokenMiddleware)
+router.get("/card", tokenMiddleware, getAllCardsByUserId)
 
 export default router;
