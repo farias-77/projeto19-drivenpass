f706d6e9f89983fb0d469d5f47b850e5ccd8646e
farias-77 <gabrielfalves2003@gmail.com>
2022-09-10 15:28:08 -0300

    expirationDate fix in schema

--- src/schemas/cardSchemas.ts
+++ src/schemas/cardSchemas.ts
@@ -4,8 +4,8 @@ export const cardCreationSchema = joi.object({
     title: joi.string().required(),
     cardNumber: joi.string().required(),
     cardHolderName: joi.string().required(),
-    securityCode: joi.string().required(),
-    expirationDate: joi.string().required(),
+    securityCode: joi.number().required(),
+    expirationDate: joi.string().pattern(/^[0-9]{2}\/[0-9]{2}$/).message("Por favor, utilize o formato MM/YY.").required(),
     password: joi.string().required(),
     isVirtual: joi.boolean().required(),
     type: joi.string().valid('credit', 'debit', 'both')
